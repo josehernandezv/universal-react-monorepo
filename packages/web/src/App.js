f0b7393fd1a2d1a239280253240b53088234c99b
josehernandezv <jose.andres.art@gmail.com>
2020-09-04 11:49:50 -0600

    set up react-native-web

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,17 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { getWelcomeString } from 'common/src/main';
-
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>{getWelcomeString('World')}</p>
-            </header>
-        </div>
-    );
-}
+import App from 'common/src/App';
 
 export default App;
